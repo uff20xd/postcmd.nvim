6f9f2ab22c81bb718b5c23539228b6eab167db31
uff20xd <donut121008@gmail.com>
2026-01-21 21:20:57 +0100

    k

--- lua/init.lua
+++ lua/init.lua
@@ -5,9 +5,23 @@ local PostCmd = {}
 
 local vapi = vim.api
 
+PostCmd.open = function()
+  if PostCmd.binding_file == NULL then
+    return
+  end
+  if PostCmd.show then
+    PostCmd.show = false
+  else
+    PostCmd.win = vapi.nvim_open_win(PostCmd.buf, true, {relative='win', row=3, col=3, width=40, height=4})
+    PostCmd.show = true
+  end
+end
 
 PostCmd.setup = function()
   PostCmd.buf = vapi.nvim_create_buf(true, true)
+  PostCmd.binding_file = "/tmp/postcmd-session.sh"
   vapi.nvim_buf_set_name(buf, "postcmd")
 end
+
+return PostCmd
 
