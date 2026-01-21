53c6c07bf5000ec31e7f32f94d9c64b8c73a3410
uff20xd <donut121008@gmail.com>
2026-01-21 11:35:40 +0100

    k

--- lua/init.lua
+++ lua/init.lua
@@ -1,5 +1,11 @@
 -- Buffer saved in /tmp or in the current dir
 -- You can edit commands as a buffer for testing things quickly.
 -- Executes vim commands, not shell commands
+local PostCmd = {}
+
+local vapi = vim.api
+
+PostCmd.buf = vapi.nvim_create_buf(true, true)
+vapi.nvim_buf_set_name(buf, "postcmd")
 
 
